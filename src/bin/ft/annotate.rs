2e6a64b5dc041fa12257e9b4a21c3779647fa784
atks <atks@umich.edu>
2024-10-25 16:46:02 +0800

    misc changes for annotate

--- src/bin/ft/annotate.rs
+++ src/bin/ft/annotate.rs
@@ -18,10 +18,7 @@
 
 pub(crate) use clap::Args;
 extern crate needletail;
-use gb_io::reader::SeqReader;
-use gb_io::seq::Location;
 use std::fs::File;
-use string_interner::StringInterner;
 
 #[derive(Debug, Args)]
 pub struct AnnotateArgs {
@@ -34,148 +31,13 @@ pub struct AnnotateArgs {
     input_fasta_file: Option<String>,
 }
 
-/// .
-#[doc = r"Annotate a sequence using a reference genbank record"]
-pub fn run(options: AnnotateArgs) {
-    //process arguments
-    println!("Annotating sequence");
-
-    let genbank_file;
-
-    match options.genbank_file {
-        Some(filename) => {
-            genbank_file = File::open(filename).unwrap();
-        }
-        None => {
-            genbank_file = File::open("/dev/stdin").unwrap();
-        }
+pub fn run(args: AnnotateArgs) {
+    let genbank_file = File::open(args.genbank_file.unwrap()).unwrap();
+    let input_fasta_file = File::open(args.input_fasta_file.unwrap()).unwrap();
+    let reader = needletail::parse_genbank(genbank_file).unwrap();
+    let mut writer = needletail::parse_fasta(input_fasta_file).unwrap();
+    for record in reader {
+        let record = record.unwrap();
+        writer.write_record(&record).unwrap();
     }
-
-    let mut interner = StringInterner::default();
-    //interned strings
-    let gene = interner.get_or_intern("gene");
-
-    //read genbank file
-    //we only expect one sequence entry in the genbank file
-    for seq in SeqReader::new(genbank_file) {
-        let seq1 = seq.unwrap();
-
-        let acc = seq1.accession.unwrap_or(String::from("default"));
-        println!("accession: {}", acc);
-
-        //process features
-        let mut no_features = 0;
-        for feature in seq1.features {
-            println!("kind: {:?}", feature.kind);
-            println!("location: {:?}", feature.location);
-            match feature.location {
-                Location::Range(x, y) => {
-                    println!("range: {:?} {:?}", x, y);
-                    println!("range: {:?} {:?}", x.0, y.0);
-                }
-                Location::OneOf(one_of) => {
-                    println!("one_of: {:?}", one_of);
-                }
-                Location::Between(x, y) => {
-                    println!("between: {:?} {:?}", x, y);
-                }
-                Location::Complement(x) => {
-                    println!("complement: {}", *x);
-                    println!("complement: {}", x);
-                }
-                Location::Join(join) => {
-                    println!("join: {:?}", join);
-                }
-                Location::Order(order) => {
-                    println!("order: {:?}", order);
-                }
-                Location::Bond(order) => {
-                    println!("bond: {:?}", order);
-                }
-                Location::External(x, y) => {
-                    println!("external: {:?} {:?}", x, y);
-                }
-                Location::Gap(gap) => {
-                    println!("gap: {:?}", gap);
-                }
-                _ => println!("Not handled!"),
-            }
-            for qualifier in feature.qualifiers {
-                match qualifier {
-                    (x, y) => {
-                        println!("x: {:?}", x);
-                        println!("y: {:?}", y);
-                    }
-                    // gb_io::seq::QualifierKey::Allele(allele) => {
-                    //     println!("\tallele: {:?}", allele);
-                    // }
-                    // gb_io::record::Qualifier::Citation(citation) => {
-                    //     println!("\tcitation: {:?}", citation);
-                    // }
-                    // gb_io::record::Qualifier::Codon(codon) => {
-                    //     println!("\tcodon: {:?}", codon);
-                    // }
-                    // gb_io::record::Qualifier::Compare(compare) => {
-                    //     println!("\tcompare: {:?}", compare);
-                    // }
-                    // gb_io::record::Qualifier::Country(country) => {
-                    //     println!("\tcountry: {:?}", country);
-                    // }
-                    // gb_io::record::Qualifier::DbXref(dbxref) => {
-                    //     println!("\tdbxref: {:?}", dbxref);
-                    // }
-                    // gb_io::record::Qualifier::Direction(direction) => {
-                    //     println!("\tdirection: {:?}", direction);
-                    // }
-                    // gb_io::record::Qualifier::EC_number(ec_number) => {
-                    //     println!("\tec_number: {:?}", ec_number);
-                    // }
-                    // gb_io::record::Qualifier::Experiment(experiment) => {
-                    //     println!("\texperiment: {:?}", experiment);
-                    // }
-                    // gb_io::record::Qualifier::Function(function) => {
-                    //     println!("\tfunction: {:?}", function);
-                    // }
-                    // gb_io::seq::QualifierKeyStatisticSet::Gene(gene) => {
-                    //     println!("\tgene: {:?}", gene);
-                    // }
-                    // gb_io::record::Qualifier::GenMapDbXref(gen_map_dbxref) => {
-                    //     println!("\tgen_map_dbxref: {:?}", gen_map_dbxref);
-                    // }
-                    // gb_io::record::Qualifier::Inference(inference) => {
-                    //     println!("\tinference: {:?}", inference);
-                    // }
-                    // gb_io::record::Qualifier::Isolate(isolate) => {
-                    //     println!("\tisolate: {:?}", isolate);
-                    // }
-                    // gb_io::record::Qualifier::Label(label) => {
-                    //     println!("\tlabel: {:?}", label);
-                    // }
-                    // gb_io::record::Qualifier::LocusTag(locus_tag) => {
-                    //     println!("\tlocus_tag: {:?}", locus_tag);
-                    // }
-                    // gb_io::record::Qualifier::Map(map) => {
-                    //     println!("\tmap: {:?}", map);
-                    // }
-                    // gb_io::record::Qualifier::MobileElement(mobile_element) => {
-                    //     println!("\tmobile_element: {:?}", mobile_element);
-                    // }
-                    _ => println!("\tqualifier: {:?}", qualifier),
-                }
-            }
-            println!("\n\n");
-            no_features += 1;
-        }
-
-        println!("no of features: {}", no_features);
-    }
-
-    //read fasta file
-
-    //annotate
-    //blast features to reference
-    //check for frameshifts, nonsense mutations, etc etc
-    //transfer annotations
-
-    //write annotated fasta file
 }
